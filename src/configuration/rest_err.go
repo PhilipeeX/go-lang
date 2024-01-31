3d0cec540c344e796c0fc1879cc80124523308b3
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-31 08:49:25 -0300

    build(project): Go boilerplate

---
+++ src/configuration/rest_err.go
@@ -0,0 +1,18 @@
+package rest_err
+
+//
+//type RestErr struct {
+//	Message string   `json:"message"`
+//	Err     string   `json:"error"`
+//	Code    int      `json:"code"`
+//	Causes  []Causes `json:"causes"`
+//}
+//
+//type Causes struct {
+//	Field   string `json:"field"`
+//	Message string `json:"message"`
+//}
+//
+//func NewRestErr(message, err string, code int, causes []Causes) {
+//
+//}
